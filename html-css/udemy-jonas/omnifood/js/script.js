8db339424f8a11c684ca320de5a4344beda24d2a
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-03-03 19:24:41 +0100

    omnifood - java script - mobile navigation

---
+++ html-css/udemy-jonas/omnifood/js/script.js
@@ -0,0 +1,6 @@
+/* Mobile navigation */
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", () => headerEl.classList.toggle("nav-open"));
