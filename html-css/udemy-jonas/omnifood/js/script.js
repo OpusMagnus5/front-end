f0747d02c7109a388c5142fcea90720c8f777930
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-03-04 15:19:55 +0100

    omnifood - java script - smooth scroll

--- html-css/udemy-jonas/omnifood/js/script.js
+++ html-css/udemy-jonas/omnifood/js/script.js
@@ -4,3 +4,21 @@ const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", () => headerEl.classList.toggle("nav-open"));
+
+/* Scrolling animation */
+
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach(link => link.addEventListener('click', event => {
+    event.preventDefault();
+    const href = link.getAttribute('href');
+    if (href === '#') {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    } else if (href !== '#' && href.startsWith('#')) {
+        const sectionEl = document.querySelector(href);
+        sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    if (link.classList.contains('main-nav-link')) {
+        headerEl.classList.toggle("nav-open")
+    }
+}));
